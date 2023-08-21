8ba80ff0a186e05e5f34b80e0210e254267ebf0e
Rofer <roferhn@gmail.com>
2023-08-21 13:22:46 -0600

    Redesigned  models

--- Rapienvios/rapienviosBackend/models.py
+++ Rapienvios/rapienviosBackend/models.py
@@ -3,39 +3,70 @@ from django.db import models
 # Create your models here.
 
 # ---User Models--#
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.TextField(null=False) #Investigar e implementar hashing
-    phone = models.CharField(max_length=8)
-    locker = models.CharField(max_length=10)
+class User_Type(models.Model):
+    type = models.CharField(max_length=50)
     status = models.BooleanField(default=True)
-    
-    USER_TYPES = (
-        ('admin', 'Admin'),
-        ('op','Operator'),
-        ('client','Client')
-    )
-    type = models.CharField(max_length=10, choices=USER_TYPES)
 
+    def __str__(self):
+        return self.type
+
+class User(models.Model):
+       
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.TextField(min_length=6) #Investigar e implementar hashing
+    phone = models.CharField(min_length=8, max_length=8)
+    user_type= models.ForeignKey(User_Type, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)    
+    
+    def __str__(self):
+        return self.type
+
+# --- Shipping Models---#
+class Shipping_Status(models.Model):
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.status
+
+class Shipping_Type(models.Model):
+    type = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.type
+
+class Shipping(models.Model):
+    locker = models.CharField(max_length=10, null=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    shipping_date = models.DateField()
+    delicery_date = models.DateField()
+    updated_date = models.DateField()
+    shipping_type_id = models.ForeignKey(Shipping_Type, on_delete=models.CASCADE)
+    volume = models.DecimalField(max_digits=10, decimal_places=2)
+    weigth = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_status_id = models.ForeignKey(Shipping_Status, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.type
+
+class Pricing(models.Model):
+    shipping_status_id = models.ForeignKey(Shipping_Status, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    initial_date = models.DateField()
+    final_date = models.DateField()
 
 # ---Package Models---#
 class Package(models.Model):
-    tracking_num = models.TextField()
-    locker = models.IntegerField()
-    weight = models.DecimalField(decimal_places=2,max_digits=4)
-    reception_date = models.DateField()
-    packaged_date = models.DateField()
+    tracking_num = models.CharField(max_length=100)
+    reception_date = models.DateField(null=False )
+    packaged_date = models.DateField(null=False )
+    shipping_id = models.ForeignKey(Shipping,)
 
-# --- Shipping Models---#
-class Shipping(models.Model):
-    delicery_date = models.DateField()
-    updated_date = models.DateField()
+    def __str__(self):
+        return self.type
 
-    SHIPPING_STATUS =(
-        ('1','Ready to ship'),
-        ('2','Shipped'),
-        ('3','Delivered at office')
-    )
-    status = models.CharField(max_length=100,choices=SHIPPING_STATUS)
+class Locker(models.Model):
+    package_id = models.ForeignKey(Package, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
